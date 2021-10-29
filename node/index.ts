4b1ddb9d53244bad568418816dd56d07c5626fd9
Jorge Espinosa Aldana <jorgeespinosa@Jorges-MacBook-Air.local>
2021-10-29 17:25:18 -0300

    test1

--- node/index.ts
+++ node/index.ts
@@ -1,5 +1,6 @@
 import type { ClientsConfig, ServiceContext, RecorderState } from '@vtex/api'
-import {LRUCache, method, Service, UserInputError} from '@vtex/api'
+import { LRUCache, method, Service, UserInputError } from '@vtex/api'
+import axios from 'axios'
 
 import { Clients } from './clients'
 import { status } from './middlewares/status'
@@ -69,29 +70,11 @@ export async function email(ctx: Context, next: () => Promise<any>) {
     throw new UserInputError('Code is required') // Wrapper for a Bad Request (400) HTTP Error. Check others in https://github.com/vtex/node-vtex-api/blob/fd6139349de4e68825b1074f1959dd8d0c8f4d5b/src/errors/index.ts
   }
 
-  const https = require('https')
+  const resp = await axios.get(
+    'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+  )
 
-  const data = JSON.stringify({
-    todo: 'Buy the milk'
-  })
-
-  const options = {
-    hostname: 'https://2ec5f9c9-c085-4dd0-acd4-06aa6c59c8ad.mock.pstmn.io',
-    port: 443,
-    path: '/giftcards/_search',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length
-    }
-  }
-
-  const req = await https.request(options)
-
-  ctx.body = req
-
-  ctx.state.code = 201
+  ctx.state.code = resp.data
 
   await next()
 }
-
